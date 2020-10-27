935ff5ce60f53220fc63e475557271da041b9538
Sergey <signatuk89@gmail.com>
2020-10-28 00:20:37 +0300

    employee dao

---
+++ src/main/java/com/dao/jdbc/JdbcEmployeeDaoImpl.java
@@ -0,0 +1,89 @@
+package com.dao.jdbc;
+
+import com.dao.EmployeeDao;
+import com.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class JdbcEmployeeDaoImpl implements EmployeeDao {
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @Autowired
+    public JdbcEmployeeDaoImpl(DataSource dataSource) {
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    private RowMapper<Employee> rowMapper() {
+        return (rs, i) -> {
+            Employee employee = new Employee();
+            employee.setId(rs.getLong("id"));
+            employee.setFirstName(rs.getString("first_name"));
+            employee.setLastName(rs.getString("last_name"));
+            employee.setDepartmentId(rs.getString("department_id"));
+            employee.setJobTitle(rs.getString("job_title"));
+            employee.setGender(rs.getString("gender"));
+            employee.setDateOfBirth(rs.getDate("date_of_birth"));
+            return employee;
+        };
+    }
+
+    @Override
+    public List<Employee> findAllEmployees() {
+        String sql = "SELECT id, first_name, last_name, department_id, job_title, gender, date_of_birth " +
+                "FROM employees";
+        return namedParameterJdbcTemplate.query(sql, rowMapper());
+    }
+
+    @Override
+    public Employee findEmployeeById(Long id) {
+        String sql = "SELECT id, first_name, last_name, department_id, job_title, gender, date_of_birth " +
+                "FROM employees " +
+                "WHERE id = :id";
+        Map<String, Object> params = new HashMap<>();
+        params.put("id", id);
+        return namedParameterJdbcTemplate.queryForObject(sql, params, rowMapper());
+    }
+
+    @Override
+    public void addNewEmployee(Employee employee) {
+        String sql = "INSERT INTO employees (first_name, last_name, department_id, job_title, gender, date_of_birth) " +
+                "VALUES (:firstName, :lastName, :departmentId, :jobTitle, :gender, :dateOfBirth)";
+        Map<String, Object> params = new HashMap<>();
+        params.put("firstName", employee.getFirstName());
+        params.put("lastName", employee.getLastName());
+        params.put("departmentId", employee.getDepartmentId());
+        params.put("jobTitle", employee.getJobTitle());
+        params.put("gender", employee.getGender());
+        params.put("dateOfBirth", employee.getDateOfBirth());
+        namedParameterJdbcTemplate.update(sql, params);
+    }
+
+    @Override
+    public Employee updateEmployeeById(Long id, String departmentId, String jobTitle) {
+        String sql = "UPDATE employees " +
+                "SET department_id = :departmentId, job_title = :jobTitle " +
+                "WHERE id = :id";
+        Map<String, Object> params = new HashMap<>();
+        params.put("id", id);
+        params.put("departmentId", departmentId);
+        params.put("jobTitle", jobTitle);
+        namedParameterJdbcTemplate.update(sql, params);
+        return findEmployeeById(id);
+    }
+
+    @Override
+    public void deleteEmployeeById(Long id) {
+        String sql = "DELETE FROM employees WHERE id = :id";
+        Map<String, Object> params = new HashMap<>();
+        params.put("id", id);
+        namedParameterJdbcTemplate.update(sql, params);
+    }
+}
---
+++ src/main/java/com/model/Employee.java
@@ -0,0 +1,105 @@
+package com.model;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Employee {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String departmentId;
+    private String jobTitle;
+    private String gender;
+    private Date dateOfBirth;
+
+    public Employee() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(getId(), employee.getId()) &&
+                Objects.equals(getFirstName(), employee.getFirstName()) &&
+                Objects.equals(getLastName(), employee.getLastName()) &&
+                Objects.equals(getDepartmentId(), employee.getDepartmentId()) &&
+                Objects.equals(getJobTitle(), employee.getJobTitle()) &&
+                Objects.equals(getGender(), employee.getGender()) &&
+                Objects.equals(getDateOfBirth(), employee.getDateOfBirth());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName(), getDepartmentId(), getJobTitle(), getGender(), getDateOfBirth());
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", departmentId='" + departmentId + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", gender='" + gender + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
+}
