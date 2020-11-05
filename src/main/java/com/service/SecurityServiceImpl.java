8f9de57f4e94080e32e4133b27133435e7074af6
S.Ignatyuk <signatyuk89@gmail.com>
2020-11-05 15:25:45 +0300

    log v1.0

--- src/main/java/com/service/SecurityServiceImpl.java
+++ src/main/java/com/service/SecurityServiceImpl.java
@@ -1,5 +1,7 @@
 package com.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class SecurityServiceImpl implements SecurityService {
+    private static final Logger logger = LoggerFactory.getLogger(SecurityServiceImpl.class);
     @Autowired
     private AuthenticationManager authenticationManager;
 
@@ -45,6 +48,7 @@ public class SecurityServiceImpl implements SecurityService {
 
         if (authenticationToken.isAuthenticated()) {
             SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            logger.debug("Successfully {} auto logged in", username);
         }
     }
 }
